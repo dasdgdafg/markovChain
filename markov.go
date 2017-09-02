198d7cf4c85ae6ccfade48cb17933962c7f81dbf
dsdf <dasdgdafg@cock.li>
2017-09-02 13:24:24 -0400

    remove unneeded function

--- markov.go
+++ markov.go
@@ -116,17 +116,3 @@ func (c *Chain) Generate(n int) string {
 	}
 	return strings.Replace(strings.Join(words, " "), ENDLINE, "", -1)
 }
-
-//https://stackoverflow.com/questions/33633168/how-to-insert-a-character-every-x-characters-in-a-string-in-golang
-func insertNth(s string, n int) string {
-	var buffer bytes.Buffer
-	var n_1 = n - 1
-	var l_1 = len(s) - 1
-	for i, rune := range s {
-		buffer.WriteRune(rune)
-		if i%n == n_1 && i != l_1 {
-			buffer.WriteRune(' ')
-		}
-	}
-	return buffer.String()
-}
